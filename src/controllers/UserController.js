e28f692ab3004085c9b487f3d1ed879199b028a0
joaochioda <joao.benetasso@taglivros.com>
2022-04-03 14:41:41 -0300

    coverage

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,12 +18,4 @@ module.exports = {
       return res.status(500);
     }
   },
-  async index1(req, res) {
-    try {
-      const users = await User.findAll();
-      return res.status(200).json(users);
-    } catch (ex) {
-      return res.status(500);
-    }
-  },
 };
