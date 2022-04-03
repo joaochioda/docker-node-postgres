3e2161176105aeea0772e71742d73318e8129490
joaochioda <joao.benetasso@taglivros.com>
2022-04-03 12:35:15 -0300

    tests

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,14 +2,20 @@ const User = require('../models/User')
 
 module.exports = {
     async store(req, res) {
-        const { name } = req.body;
-        const user = await User.create({name})
-
-        return res.json(user)
+        try {
+          const { name } = req.body;
+          const user = await User.create({ name });
+          return res.status(200).json(user);
+        } catch (ex) {
+          return res.status(500).json("Failed to create user")
+        }
     },
     async index(req, res) {
-        const users = await User.findAll();
-
-        return res.json(users);
-    }
+        try {
+            const users = await User.findAll();
+            return res.status(200).json(users);
+        } catch (ex) {
+          return res.status(500)
+        }
+    },
 }
